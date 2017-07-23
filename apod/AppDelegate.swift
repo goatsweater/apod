85b9c0c7388ea6699ef5fdcfe3afe59b123d3329
Reg Maltais <reginald.maltais@gmail.com>
2017-07-23 18:58:08 -0400

    Initial Commit

---
+++ apod/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  apod
+//
+//  Created by Reginald Maltais on 2017-07-23.
+//  Copyright © 2017 Reginald Maltais. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ apod/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  apod
+//
+//  Created by Reginald Maltais on 2017-07-23.
+//  Copyright © 2017 Reginald Maltais. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
